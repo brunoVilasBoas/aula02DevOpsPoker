7d523b3bc81ca3d0de4ccac0109616ebcb61e2ca
warleylimagit <warley.info@gmail.com>
2019-02-21 23:11:15 -0300

    Criando Objetos para gerar cartas e nipes.

--- Objetos/Cartas.cs
+++ Objetos/Cartas.cs
@@ -1,22 +1,36 @@
 using System;
 using System.Collections.Generic;
+using aula02DevOpsPoker.Poco;
 
 namespace aula02DevOpsPoker.Objetos
 {
     public class Cartas{
-        public List<object> CartasRetorno = new List<object>();
+        public List<Carta> CartasRetorno = new List<Carta>();
 
-        public List<object> GerarCartas(){
-            for (int i = 1; i < 9; i++)
+        public List<Carta> GerarCartas(){
+            Carta carta = new Carta();
+            string[] letras = {"J", "Q", "K", "A"};
+
+            for (int i = 1; i <= 9; i++)
             {
-                var valor = i + 1; 
-                CartasRetorno.Add(valor);
+                carta.Id = i;
+                carta.CartaNome = (i + 1).ToString();
+                carta.Peso = i;
+                CartasRetorno.Add(carta);
+                carta = new Carta();
             }
 
-            CartasRetorno.Add("J");
-            CartasRetorno.Add("Q");
-            CartasRetorno.Add("K");
-            CartasRetorno.Add("A");
+            int count = 0;
+            int peso = 9;
+            foreach (var item in letras)
+            {
+                count = count++;
+                carta.Id = count;
+                carta.CartaNome = item;
+                carta.Peso = peso++;
+                CartasRetorno.Add(carta);
+                carta = new Carta();
+            }
 
             return CartasRetorno;
         }
--- Objetos/Nipes.cs
+++ Objetos/Nipes.cs
@@ -1,16 +1,26 @@
 using System;
 using System.Collections.Generic;
+using aula02DevOpsPoker.Poco;
 
 namespace aula02DevOpsPoker.Objetos
 {
     public class Nipes{
-        public List<object> NipesRetorno = new List<object>();
+        public List<Nipe> NipesRetorno = new List<Nipe>();
 
-        public List<object> GerarNipes(){
-            NipesRetorno.Add("D");
-            NipesRetorno.Add("H");
-            NipesRetorno.Add("S");
-            NipesRetorno.Add("C");
+        public List<Nipe> GerarNipes(){
+            Nipe nipe = new Nipe();
+            string[] letras = {"D", "H", "S", "C"};
+            int count = 0;
+
+            foreach (var item in letras)
+            {                
+                nipe.Id = count;
+                nipe.NipeNome = item;
+                nipe.Peso = count;
+                NipesRetorno.Add(nipe);
+                nipe = new Nipe();    
+                count = count + 1;            
+            }
 
             return NipesRetorno;
         }
